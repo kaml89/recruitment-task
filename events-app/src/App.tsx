7e93335edb3a7149299349076acd4e5347ceba85
kamil <rba.kamil@gmail.com>
2022-10-19 12:15:02 +0200

    feat/add react-toastify notifications

--- events-app/src/App.tsx
+++ events-app/src/App.tsx
@@ -1,10 +1,15 @@
 import AddEvent from "./pages/add-event/AddEvent"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   return (
     <div className="App">
       <AddEvent />
+      <ToastContainer
+        autoClose={false}
+        />
     </div>
   )
 }
